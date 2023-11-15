bf42bba2be1d086178c163819bf82c458dd7fd31
rzpy1997 <kingradomir@gmail.com>
2023-11-15 09:15:47 +0600

    first_lesson

---
+++ main_page/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import RunString, FilmPostModel, Afisha
+
+admin.site.register(RunString)
+admin.site.register(FilmPostModel)
+admin.site.register(Afisha)
---
+++ main_page/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404
+from .models import RunString, FilmPostModel, Afisha
+
+def string_post_view(request):
+    if request.method == 'GET':
+        string_ = RunString.objects.all()
+        film_list = FilmPostModel.objects.all()
+        afisha = Afisha.objects.all()
+        return render(request, template_name='main_page/index.html',
+                      context={
+                                 'string_': string_,
+                                 'film_list': film_list,
+                                 'afisha': afisha,
+                      })
+
+
+def film_detail_view(request, id):
+    if request.method == 'GET':
+        film_id = get_object_or_404(FilmPostModel, id=id)
+        return render(request, template_name='main_page/film_detail.html', context={'film_id': film_id})
